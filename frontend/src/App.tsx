11bc85f8f08c1f0ff0b88011dd9b106a43c6a91b
tsviatoslav <torba.svyatoslav.fox@gmail.com>
2022-12-21 12:46:49 +0200

    updated some settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function App() {
+  let getNotes = async () => {
+    let response = await fetch('http://127.0.0.1:8000/api/notes')
+    let data = await response.json()
+    console.log(data)
+  }
+  useEffect(() => {
+    getNotes()
+  }, [])
   return (
-    <h1 className="mt-52 font-bold">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="mt-52 font-bold">
+        Hello world!
+      </h1>
+    </>
 
   );
 }
